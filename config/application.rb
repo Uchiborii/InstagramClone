99b186d36e5f9f797392b9cf348961eb93c1562d
uchiborieri <zuumo219@gmail.com>
2023-06-06 15:57:39 +0900

    インデント調整

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative "boot"
 
 require "rails/all"
 Bundler.require(*Rails.groups)
+config.time_zone = 'Tokyo'
+config.active_record.default_timezone = :local
 
 module InstagramCloneApp
   class Application < Rails::Application
