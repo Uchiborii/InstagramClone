9d4ac05558501cfda83a4ad87595f3ac6105dbf1
uchiborieri <zuumo219@gmail.com>
2023-06-08 12:40:42 +0900

    config重複削除

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative "boot"
 
 require "rails/all"
 Bundler.require(*Rails.groups)
-config.time_zone = 'Tokyo'
-config.active_record.default_timezone = :local
 
 module InstagramCloneApp
   class Application < Rails::Application
